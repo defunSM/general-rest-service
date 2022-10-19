924c01271965cef04b6b09070b7e3325e96ca7f7
defunsm <salmanhossain500@gmail.com>
2022-10-19 02:11:52 -0400

    add: unit tests for helpers.py

---
+++ tests/countleaf/test_helpers.py
@@ -0,0 +1,26 @@
+""" Pytest for helpers.py"""
+# pylint: disable=[missing-function-docstring]
+
+def test_strip_punctuation():
+    pass
+
+def test_strip_gaps():
+    pass
+
+def test_strip_articles():
+    pass
+
+def test_count_frequent_word():
+    pass
+
+def test_count_sentences():
+    pass
+
+def test_count_letters():
+    pass
+
+def test_count_words():
+    pass
+
+def test_extract_text_from_url():
+    pass
